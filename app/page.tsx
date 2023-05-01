b469eac076e331fb25e403d71feb5021c05af24b
MBrian Website <muhammad.brian.27@gmail.com>
2023-05-02 06:41:53 +0700

    SET UP MAIN PAGE COMPONENT

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import NavBar from "./components/NavBar";
 
-import Link from "next/link";
-import SearchBar from "./components/SearchBar";
+import Header from "./components/Header";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
@@ -12,41 +12,12 @@ export default function Home() {
         {/* NAVBAR */}
         <main>
           {/* HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-              {/* SEARCH BAR */}
-              <SearchBar />
-              {/* SEARCH BAR */}
-            </div>
-          </div>
-          {/* HEADER */} {/* CARDS */}
+          <Header />
+          {/* HEADER */}
+          {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
             {/* CARD */}
-            <Link href={"/restaurant/go-go"}>
-              <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-                <img
-                  src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-                  alt=""
-                  className="w-full h-36"
-                />
-                <div className="p-1">
-                  <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-                  <div className="flex items-start">
-                    <div className="flex mb-2">*****</div>
-                    <p className="ml-2">77 reviews</p>
-                  </div>
-                  <div className="flex text-reg font-light capitalize">
-                    <p className=" mr-3">Mexican</p>
-                    <p className="mr-3">$$$$</p>
-                    <p>Toronto</p>
-                  </div>
-                  <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-                </div>
-              </div>
-            </Link>
+            <Card />
             {/* CARD */}
           </div>
           {/* CARDS */}
