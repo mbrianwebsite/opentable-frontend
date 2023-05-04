15eca74f747d0cf6c2f8edaa1aa3965b8c737293
MBrian Website <muhammad.brian.27@gmail.com>
2023-05-04 19:35:30 +0700

    SET UP RESERVE FORM STATE

--- app/reserve/[slug]/components/Form.tsx
+++ app/reserve/[slug]/components/Form.tsx
@@ -1,35 +1,57 @@
+"use client";
+import { useState } from "react";
+
 export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [occasion, setOccasion] = useState("");
+  const [request, setRequest] = useState("");
+
   return (
     <div className="mt-10 flex flex-wrap justify-between w-[660px]">
       <input
         type="text"
         className="border rounded p-3 w-80 mb-4"
         placeholder="First name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
       />
       <input
         type="text"
         className="border rounded p-3 w-80 mb-4"
         placeholder="Last name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
       />
       <input
         type="text"
         className="border rounded p-3 w-80 mb-4"
         placeholder="Phone number"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
       />
       <input
-        type="text"
+        type="email"
         className="border rounded p-3 w-80 mb-4"
         placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="text"
         className="border rounded p-3 w-80 mb-4"
         placeholder="Occasion (optional)"
+        value={occasion}
+        onChange={(e) => setOccasion(e.target.value)}
       />
       <input
         type="text"
         className="border rounded p-3 w-80 mb-4"
         placeholder="Requests (optional)"
+        value={request}
+        onChange={(e) => setRequest(e.target.value)}
       />
       <button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
         Complete reservation
